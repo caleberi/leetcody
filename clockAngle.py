1606fe11a89fa951bc6fab38e64db0d9f90b93a4
Caleb Adewole <caleberioluwa@gmail.com>
2021-07-21 10:10:49 +0100

    inital code base

---
+++ clockAngle.py
@@ -0,0 +1,10 @@
+def clockAngle():
+    # Given the hours and the minutes on an analog clock, return the angle formed between the two hands.
+    # Note: Return the smaller of the two angles formed.
+    # Ex: Given the following hours and minutes...
+    # hours = 12, minutes = 0, return 0 (the hands are on top of each other).
+    # Ex: Given the following hours and minutes...
+    # hours = 3, minutes = 25, return 47.5
+    # hours = 4, minutes = 50, return 90
+    # hours = 12, minutes = 0, return 0
+    pass
