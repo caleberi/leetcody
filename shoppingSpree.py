ad97051f4931ca721d3308268b4de60f221dacfa
Caleb Adewole <caleberioluwa@gmail.com>
2021-07-27 16:35:20 +0100

    added question to shoppingSpree

--- shoppingSpree.py
+++ shoppingSpree.py
@@ -1,3 +1,19 @@
+# You are given an integer array that represents the prices of items in a store.
+# The ith price is given by prices[i]; however, the store is running a special discount.
+#  By buying the ith item, you receive a discount of prices[j] where prices[j] is less than or equal to prices[i] and j > i.
+#  If no such prices[j] exists, you pay for price for the ith item.
+# Given these prices, return an array that represents the amount you’ll pay for each respective item considering the special discount.
+
+# Ex: Given the following prices…
+# prices = [3, 2, 2], return [1, 0, 2].
+# For prices[0] you pay 3 - 1 = 1 dollars.
+# For prices[1] you pay 2 - 2 = 0 dollars.
+# For prices[2] you pay 2 - 2 = 0 dollars.
+
+############################################################################################
+# Path: shoppingSpree.py
+#################################
+# generate test cases
 def shoppingSpree(prices):
     if len(prices) == 0 or len(prices) == 1:
         return prices
