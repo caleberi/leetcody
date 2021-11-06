66d110430915eabc62a55f76d95580fa050874c5
caleberi <caleberioluwa@gmail.com>
2021-11-06 08:08:55 +0100

    added another solution to singl number using XOR

--- cpp/singleNumber.cpp
+++ cpp/singleNumber.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
+    // method 1
+    // vector<int> singleNumber(vector<int>& nums) {
+    //     unordered_map<int,int> num_to_freq;
+    //     unordered_map<int,int> freq_to_num;
+    //     for(int num:nums){
+    //         if(num_to_freq.find(num)==num_to_freq.end())
+    //             num_to_freq[num] = 1;
+    //         else
+    //             num_to_freq[num]++;
+    //     }
+
+    //     vector<int> ret;
+    //     for (int num:nums){
+    //         if(num_to_freq[num]==1){
+    //            ret.push_back(num);
+    //         }
+    //     }
+
+    //     return ret;
+    // }
     vector<int> singleNumber(vector<int>& nums) {
-        unordered_map<int,int> num_to_freq;
-        unordered_map<int,int> freq_to_num;
-        for(int num:nums){
-            if(num_to_freq.find(num)==num_to_freq.end())
-                num_to_freq[num] = 1;
-            else
-                num_to_freq[num]++;
-        }
-
         vector<int> ret;
-        for (int num:nums){
-            if(num_to_freq[num]==1){
-               ret.push_back(num);
+        sort(nums.begin(),nums.end());
+        int i=0;
+        for(;i<nums.size()-1;){
+            int cur = nums[i];
+            int pcr = nums[i+1];
+            if ((pcr^cur)!=0){
+                ret.push_back(nums[i]);
+                i+=1;
+                continue;
             }
+            i+=2;
         }
-
+        if(i==nums.size()-1){
+            int cur = nums[i];
+            int pcr = nums[i-1];
+            if ((pcr^cur)!=0)
+                ret.push_back(nums[i]);
+        }
         return ret;
     }
 };
