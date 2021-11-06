6c13c9dbfdde9bc1d1367e9aef78dfed4703b2e7
caleberi <caleberioluwa@gmail.com>
2021-11-06 13:51:35 +0100

    solution todays algo problem

---
+++ i_algorithms/sorted_square_array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int i=0;
+        for(;i<nums.size();i++){
+            nums[i]*=nums[i];
+        }
+        sort(nums.begin(),nums.end());
+        return nums;
+    }
+};
