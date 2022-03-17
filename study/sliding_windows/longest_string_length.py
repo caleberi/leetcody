7a2c32cea255f62c6affc7a6efa987c8f7ca098c
caleberi <caleberioluwa@gmail.com>
2022-03-17 01:51:37 +0100

    longest_substring.py solution

---
+++ study/sliding_windows/longest_string_length.py
@@ -0,0 +1,24 @@
+
+# vbsdinoooki
+
+def find_longest_substring(string):
+    start = 0
+    end = 0
+    max_len = 0
+    seen = {}
+    while start < len(string) and end < len(string):
+        current = string[end]
+        if current not in seen:
+            seen[current] = end
+        elif current in seen:
+            temp = end-start
+            start = seen[current]+1
+            max_len =  max(temp,max_len)
+        end += 1
+    return max_len
+
+print(find_longest_substring("vbsdinoooki"))
+            
+print(find_longest_substring("talentupafrica"))
+        
+
