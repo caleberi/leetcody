4db04ee266e3d7647e1969bf7c7379a892fea84c
caleberi <caleberioluwa@gmail.com>
2022-04-26 01:16:03 +0100

    solution to valid palindrome

---
+++ java/com/leetcode/ValidPalindrome.java
@@ -0,0 +1,29 @@
+package java.com.leetcode;
+
+public class ValidPalindrome {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while( i < j){
+            if ( s.charAt(i) != s.charAt(j) ){
+                return checkPalindrome(s,i,j-1) || checkPalindrome(s,i+1,j);
+            }
+           i += 1;
+           j -= 1; 
+        }
+        return true;
+       
+    }
+
+    private boolean checkPalindrome(String s, int i, int j){
+        while( i < j){
+            if ( s.charAt(i) != s.charAt(j) ){
+                return false;
+            }
+           i += 1;
+           j -= 1; 
+        }
+        return true;
+    }
+    
+}
