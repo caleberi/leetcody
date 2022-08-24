279d03bc975244b31998f0fb13ccf75347ede379
caleberi <caleberioluwa@gmail.com>
2022-08-24 09:51:33 +0100

    add comment

--- python/isPowerOfThree.py
+++ python/isPowerOfThree.py
@@ -8,4 +8,7 @@ class Solution:
         return self.isPowerOfThree(n/3)
     
     def isPowerOfThree(self, n: int) -> bool:
+        # Any integer number other than power of 3 which divides 
+        # highest power of 3 value that integer can hold 3^19 = 1162261467 
+        # (Assuming that integers are stored using 32 bits) will give reminder non-zero.
         return n > 0 and 1162261467 % n == 0
