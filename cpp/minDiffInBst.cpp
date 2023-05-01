65c72132ff007a989f82c82543ef29e52b3912ab
Caleb Adewole Erioluwa <caleberioluwa@gmail.com>
2023-05-01 18:47:23 +0100

    feat: solution to min diff btw BST

--- cpp/minDiffInBst.cpp
+++ cpp/minDiffInBst.cpp
@@ -13,6 +13,19 @@
 class Solution {
 public:
     int minDiffInBST(TreeNode* root) {
-        
+        signed int  minDistance = 100000;
+        minDiffInBSTHelper(root, &minDistance);
+        return minDistance;
+    }
+
+    void minDiffInBSTHelper(TreeNode* parent,TreeNode* root, int* minDistance){
+        if (root == nullptr )
+            return;
+        int diff = abs(parent->val - root->val);
+        *minDistance = min(diff,*minDistance);
+        if (root->left != nullptr)
+            this->minDiffInBSTHelper(root,root->left,minDistance);
+        if (root->right != nullptr)
+            this->minDiffInBSTHelper(root,root->right,minDistance);
     }
 };
